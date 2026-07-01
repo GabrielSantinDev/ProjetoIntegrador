9405c97bda088328d28531a6f22accbf04129565
Gabriel <gabicompe@gmail.com>
2026-07-01 11:48:49 -0300

    Fix Controllers

--- src/main/java/br/edu/ifpr/bsi/projetoexemplo/controllers/AlunoController.java
+++ src/main/java/br/edu/ifpr/bsi/projetoexemplo/controllers/AlunoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/alunos")
 public class AlunoController {
 
--- src/main/java/br/edu/ifpr/bsi/projetoexemplo/controllers/CursoController.java
+++ src/main/java/br/edu/ifpr/bsi/projetoexemplo/controllers/CursoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/cursos")
 public class CursoController {
 
